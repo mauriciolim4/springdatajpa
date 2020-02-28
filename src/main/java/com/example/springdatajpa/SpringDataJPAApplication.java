30d327c461ee7e868f98d1d9cebf5a00da9230da
LIMA <mauriciolima@dxc.com>
2020-02-28 15:25:55 -0300

    Teste git changes

--- src/main/java/com/example/springdatajpa/SpringDataJPAApplication.java
+++ src/main/java/com/example/springdatajpa/SpringDataJPAApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Slf4j
+// teste
 public class SpringDataJPAApplication {
 
   public static void main(String[] args) {
